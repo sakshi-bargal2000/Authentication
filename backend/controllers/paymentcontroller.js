c6856bbef6dfc61f2697a1002621964f4d5dd3fd
Sakshi <sakshibargal2000@gmail.com>
2026-07-02 17:28:09 +0530

    online pay

--- backend/controllers/paymentcontroller.js
+++ backend/controllers/paymentcontroller.js
@@ -7,3 +7,13 @@ export const payment = TryCatch(async(req,res)=>{
     });
 
 });
+
+export const onlinepay = TryCatch(async(req,res)=>{
+
+    console.log("paymentController")
+
+    res.status(200).json({
+        message:"Token refreshed"
+    });
+
+});
